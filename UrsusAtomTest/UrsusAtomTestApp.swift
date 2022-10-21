c67ce3ff836b85754ba37a64cb89622dbddf399c
libhut-samwes <81414398+libhut-samwes@users.noreply.github.com>
2022-10-21 15:27:15 +0800

    Initial Commit

---
+++ UrsusAtomTest/UrsusAtomTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrsusAtomTestApp.swift
+//  UrsusAtomTest
+//
+//  Created by Libhut Samwes on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct UrsusAtomTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
